86a24f1502b1f20a8147214739900f3042412c49
Reshka1112 <58931986+Reshka1112@users.noreply.github.com>
2020-03-15 17:33:46 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,86 @@
+let btn1 = document.getElementById('firstblock');
+let block1 = document.getElementById('article1');
+let btn2 = document.getElementById('secondblock');
+let block2 = document.getElementById('article2');
+let btn3 = document.getElementById('thirdblock');
+let block3 = document.getElementById('article3');
+let btn4 = document.getElementById('fourthblock');
+let block4 = document.getElementById('article4');
+let btn5 = document.getElementById('fifthblock');
+let block5 = document.getElementById('article5');
+let btn6 = document.getElementById('sixthblock');
+let block6 = document.getElementById('article6');
+let btn7 = document.getElementById('seventhblock');
+let block7 = document.getElementById('article7');
+
+
+btn1.onclick= click1;
+function  click1(){
+    block1.style.display= 'flex';
+    block2.style.display= 'none';
+    block3.style.display= 'none';
+    block4.style.display= 'none';
+    block5.style.display= 'none';
+    block6.style.display= 'none';
+    block7.style.display= 'none';
+}
+btn2.onclick= click2;
+function  click2(){
+    block1.style.display= 'none';
+    block2.style.display= 'flex';
+    block3.style.display= 'none';
+    block4.style.display= 'none';
+    block5.style.display= 'none';
+    block6.style.display= 'none';
+    block7.style.display= 'none';
+}
+btn3.onclick= click3;
+function  click3(){
+    block1.style.display= 'none';
+    block2.style.display= 'none';
+    block3.style.display= 'flex';
+    block4.style.display= 'none';
+    block5.style.display= 'none';
+    block6.style.display= 'none';
+    block7.style.display= 'none';
+}
+btn4.onclick= click4;
+function  click4(){
+    block1.style.display= 'none';
+    block2.style.display= 'none';
+    block3.style.display= 'none';
+    block4.style.display= 'flex';
+    block5.style.display= 'none';
+    block6.style.display= 'none';
+    block7.style.display= 'none';
+}
+btn5.onclick= click5;
+function  click5(){
+    block1.style.display= 'none';
+    block2.style.display= 'none';
+    block3.style.display= 'none';
+    block4.style.display= 'none';
+    block5.style.display= 'flex';
+    block6.style.display= 'none';
+    block7.style.display= 'none';
+}
+btn6.onclick= click6;
+function  click6(){
+    block1.style.display= 'none';
+    block2.style.display= 'none';
+    block3.style.display= 'none';
+    block4.style.display= 'none';
+    block5.style.display= 'none';
+    block6.style.display= 'flex';
+    block7.style.display= 'none';
+}
+btn7.onclick= click7;
+function  click7(){
+    block1.style.display= 'none';
+    block2.style.display= 'none';
+    block3.style.display= 'none';
+    block4.style.display= 'none';
+    block5.style.display= 'none';
+    block6.style.display= 'none';
+    block7.style.display= 'flex';
+}
